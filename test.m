23178ef456ba352a50fc31db5bf49a6f8b0d59dd
FastFish523 <1729823670@qq.com>
2024-06-03 15:37:38 +0800

    Update test.m
    normal para selections; tracking q_r directly from episode 1.
    It's important that select appropriate active functions.

--- test.m
+++ test.m
@@ -7,15 +7,15 @@ Episode = 300;
 
 %% 更新律设计参数
 K1=diag([1,1,1]);
-R_u=diag([1,1,1]);
-Q_e=diag([20,20,20]);
-alpha_w=100;
-alpha_e=1;
+R_u=diag([1,1,1,1,1,1]);
+Q_e=diag([1,1,1,1,1,1]);
+alpha_w=1;
+alpha_e=0.1;
 noise_coef=0.1;
 
-num_weights = 6;
-num_states = 3;
-num_inputs = 3;
+num_weights = 8;
+num_states = 6;
+num_inputs = 6;
 
 %% 系统动态模型
 Ct = 0.3;
@@ -29,7 +29,7 @@ F_theta = @(theta) [cos(theta), -sin(theta), 0; sin(theta), cos(theta), 0; 0, 0,
 C = @(omega) [0, m*omega, 0; -m*omega, 0, 0; 0, 0, 0];
 B = [1/2, 1/2, -1; sqrt(3)/2, -sqrt(3)/2, 0; L, L, L];
 g1 = @(state) F_theta(state(3));
-f2 = @(state) -M \ (C(state(3)) * state(1:3));
+f2 = @(state) -M \ (C(state(6)) * state(4:6));
 g2 = M \ B * Ct / R;
 F = @(state) [0;0;0;f2(state)];
 I0=zeros(3,3);
@@ -92,14 +92,14 @@ eta_plot = eta_r(t_span);
 % plot(t,y_r(1,:),'r',t,y_r(2,:),'g',t,eta_actual(1,:),'r--',t,eta_actual(2,:),'g--');
 
 %% 系统状态与网络权重初始化
-z2 = [1;1;0];
+z = [1;1;0];
 
-hat_W=ones(6,1)*0.5;
+hat_W=randn(num_weights,1);
 % hat_W=rand(6,1);
 % 用于保存所有数据的参数
 all_hat_W=zeros(num_weights,Episode);
-all_e2=zeros(num_states,length(t_span));
-all_u=zeros(num_states,length(t_span));
+all_z=zeros(num_states,length(t_span));
+all_u=zeros(num_inputs,length(t_span));
 value_func=zeros(Episode,1);
 
 
@@ -108,41 +108,41 @@ all_E = ones(length(t_span), num_states);
 
 for episode = 1 : Episode
 %     z2 = [0.5;0.5;0.5];
-    z2 = randn(num_states, 1)*0.5;
+    z = randn(num_states, 1)*0.5;
     for steps = 1:length(t_span)
         t=t_span(steps);
-        if episode < 200
-            e=z2-[0;0;0];
+        if episode < 1
+            e=z-[0;0;0;0;0;0];
         else
-            e=z2-[q_r(t)];
+            e=z-[eta_r(t);q_r(t)];
         end
-        all_E(steps, :) = z2';
+        all_E(steps, :) = e';
         dPhi=Derivative_Phi(e);
-        u=-0.5*g2'*dPhi'*hat_W;
+        u=-0.5*G(z)'*dPhi'*hat_W;
         %         if episode <200
         %             u= u+(exp(-0.008*t)) * noise_coef;
         %         end
-        dz2=f2(z2)+g2*u;
+        dz=F(z)+G(z)*u;
         %                 de=g1(e2)*u;
         %         de=f2(e2)+g2*u-dot_q_r(t);
-        sigma2 = dPhi * dz2;
+        sigma2 = dPhi * dz;
         normsigma2 = sigma2' * sigma2 + 1;
 
         if (steps==1) || ((all_E(steps,:)*all_E(steps,:)' - all_E(steps-1,:)*all_E(steps-1,:)') < 0)
             dot_hat_W=alpha_w*(sigma2 / normsigma2^2) * (sigma2' * hat_W + e'*Q_e*e + u' * R_u * u);
         else
-            dot_hat_W=-0.5* alpha_e*dPhi*g2*pinv(1)*g2'*e + alpha_w*(sigma2 / normsigma2^2) * (sigma2' * hat_W + e'*Q_e*e + u' * R_u * u);
+            dot_hat_W=-0.5* alpha_e*dPhi*G(z)*pinv(R_u)*G(z)'*e + alpha_w*(sigma2 / normsigma2^2) * (sigma2' * hat_W + e'*Q_e*e + u' * R_u * u);
         end
         hat_W=hat_W-dot_hat_W/timeScale;
-        z2=z2+dz2/timeScale;
+        z=z+dz/timeScale;
 
         % 保存数据
         if steps == length(t_span)
             all_hat_W(:,episode) =hat_W;
         end
-        all_e2(:,steps) = z2;
+        all_z(:,steps) = z;
         all_u(:,steps) = u;
-        value_func(episode,1) = value_func(episode,1) + z2'*Q_e*z2 + u'*u;
+        value_func(episode,1) = value_func(episode,1) + z'*Q_e*z + u'*u;
     end
     %     set(w1_plot_handles,'XData',1:episode,'YData',all_hat_W(:,length(t_span),1:episode));
     %     drawnow;
@@ -153,43 +153,33 @@ figure;
 plot(1:Episode,all_hat_W(:,1:Episode));
 
 figure;
-plot(t_span,all_e2);
+plot(t_span,all_E);
+
+figure;
+plot(t_span,all_u);
 
 figure;
 plot(1:Episode,value_func);
 
 
 %% Functions
-function dphi=Derivative_Phi(e)
-dphi=[2*e(1),0,0;...
-    e(2),e(1),0;...
-    e(3),0,e(1);...
-    0,2*e(2),0;...
-    0,e(3),e(2);...
-    0,0,2*e(3)];
-end
-
-
 % function dphi=Derivative_Phi(e)
-% dphi=[2*e(1),0,0,0,0,0;...
-%     e(2),e(1),0,0,0,0;...
-%     e(3),0,e(1),0,0,0;...
-%     e(4),0,0,e(1),0,0;...
-%     e(5),0,0,0,e(1),0;...
-%     e(6),0,0,0,0,e(1);...
-%     0,2*e(2),0,0,0,0;...
-%     0,e(3),e(2),0,0,0;...
-%     0,e(4),0,e(2),0,0;...
-%     0,e(5),0,0,e(2),0;...
-%     0,e(6),0,0,0,e(2);...
-%     0,0,2*e(3),0,0,0;...
-%     0,0,e(4),e(3),0,0;...
-%     0,0,e(5),0,e(3),0;...
-%     0,0,e(6),0,0,e(3);...
-%     0,0,0,2*e(4),0,0;...
-%     0,0,0,e(5),e(4),0;...
-%     0,0,0,e(6),0,e(4);...
-%     0,0,0,0,2*e(5),0;...
-%     0,0,0,0,e(6),e(5);...
-%     0,0,0,0,0,2*e(6)];
+% dphi=[2*e(1),0,0;...
+%     e(2),e(1),0;...
+%     e(3),0,e(1);...
+%     0,2*e(2),0;...
+%     0,e(3),e(2);...
+%     0,0,2*e(3)];
 % end
+
+
+function dphi=Derivative_Phi(e)
+dphi=[2*e(1),0,0,0,0,0;...
+    e(4),0,0,e(1),0,0;...
+    0,2*e(2),0,0,0,0;...
+    0,e(5),0,0,e(2),0;...
+    0,0,2*e(3),0,0,0;...
+    0,0,0,2*e(4),0,0;...
+    0,0,0,0,2*e(5),0;...
+    0,0,0,0,0,2*e(6)];
+end
